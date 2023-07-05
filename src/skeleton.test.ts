135b7cbca1245c7d2799f2ea0441e761fd75160c
dani voro <mr.voro@gmail.com>
2023-07-05 11:46:40 +0200

    finished the kata

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,10 +1,12 @@
-import sum from "./skeleton";
+import betterThanAverage from "./skeleton";
 
-test("sum adds two numbers", () => {
-  expect(sum(4, 3)).toBe(7);
+test("Codewars tests", () => {
+  expect(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)).toEqual(true);
 });
 
 
-test("sum adds two numbers cancelling", () => {
-  expect(sum(-4, 4)).toBe(0);
+test.skip("sum adds two numbers cancelling", () => {
+  expect(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 69)).toEqual(true);
 });
+
+
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -1,11 +1,48 @@
 /**
- * Adds together two numbers
- * @param a - the first number to add
- * @param b - the second number to add
- * @returns the total
- */
-function sum(a: number, b: number): number {
-  return a + b;
+ *
+ * There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+INPUT: array of numbers and number
+OUTPUT: boolean value
+
+FUNCTION: betterThanAverage() 
+
+PSEUDOCODE
+
+FUNCTION betterThanAvewrage(arr, num)
+  delaring variable sumClassPoints = 0
+  declaring variable avPoint = 0
+  FOR (let number of arr) {
+    sumClassPoints += num
+  }
+  avPoint = (sumClassPoint + num) / (arr.length + 1)
+  IF num > avPoint {
+    return true
+   else 
+    return false
+  
+arr.reduce((sum, num) => sum + num, 0)
+
+
+*/
+
+function betterThanAverage(arr: number[], point: number): boolean{
+  let sumClassPoints = arr.reduce((sum, num) => sum + num, 0)
+  let avPoint = 0
+  avPoint = (sumClassPoints + point) / (arr.length + 1)
+  return (point > avPoint)   
 }
 
-export default sum;
+console.log(betterThanAverage([2, 2, 2, 2], 2))
+console.log(betterThanAverage([1, 1, 1, 1], 70))
+
+export default betterThanAverage;
